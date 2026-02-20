b258da1cef0e6b8c46185d64eb6e3a131e70fb46
Vitnesh Immanuvel <vitneshimmanuvel@gmail.com>
2026-02-20 15:02:26 +0530

    backedn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const pool = new Pool({
   },
   max: 20, // Maximum number of clients in the pool
   idleTimeoutMillis: 30000, // Close idle clients after 30 seconds
-  connectionTimeoutMillis: 2000, // Return an error after 2 seconds if connection could not be established
+  connectionTimeoutMillis: 10000, // Return an error after 10 seconds if connection could not be established
 });
 
 // Pool error handling to prevent app crash on idle client errors
